206950bd4821b9aca13cd231c4150ca5af84dd7f
ferbucheli <fbucheli@espol.edu.ec>
2021-06-27 16:45:37 -0500

    agregue todos los atributos de la clase Vehiculo

--- src/main/java/ec/edu/espol/model/Vehiculo.java
+++ src/main/java/ec/edu/espol/model/Vehiculo.java
@@ -5,15 +5,32 @@
  */
 package ec.edu.espol.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author davidperez
  */
 public class Vehiculo {
 
-  private String placa;
-  private String marca;
-  private String motor;
-  private String anio;
+    private String placa;
+    private String marca;
+    private String motor;
+    private String anio;
+    private String modelo;
+    private double recorrido;
+    private String color; 
+    private String combustible; 
+    private double precio; 
+    private ArrayList<Oferta> oferta;
+    private String correo_vendedor;
+    private Vendedor vendedor; 
+    private String vidrios;  
+    private String transmision;
+    private String tracción; 
+    private String tipo;
+    private String id_oferta;
+ 
+    public Vehiculo(Placa )
     
 }
