5fdd45bbbb0ba7220d5e6e5f4f837e0553824c42
ferbucheli <fbucheli@espol.edu.ec>
2021-06-27 22:57:09 -0500

    Agregue funciones restantes de clase: Vehiculo y Oferta

--- src/main/java/ec/edu/espol/model/Oferta.java
+++ src/main/java/ec/edu/espol/model/Oferta.java
@@ -10,5 +10,25 @@ package ec.edu.espol.model;
  * @author davidperez
  */
 public class Oferta {
+    private String id;
+    private String correo_comprador;
+    private double precio_ofertado;
+    private String placa_vehiculo;
+    private Vehiculo vehiculo;
+    private Comprador comprador;
+
+    public Oferta(String id, String correo_comprador, double precio_ofertado, String placa_vehiculo, Vehiculo vehiculo, Comprador comprador) {
+        this.id = id;
+        this.correo_comprador = correo_comprador;
+        this.precio_ofertado = precio_ofertado;
+        this.placa_vehiculo = placa_vehiculo;
+        this.vehiculo = vehiculo;
+        this.comprador = comprador;
+    }
     
+    
+    @Override
+    public String toString(){
+        return "Correo: " + this.correo_comprador + "\nPrecio Ofertado: " + this.precio_ofertado; 
+    }
 }
--- src/main/java/ec/edu/espol/model/Vehiculo.java
+++ src/main/java/ec/edu/espol/model/Vehiculo.java
@@ -7,6 +7,7 @@ package ec.edu.espol.model;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 
 /**
  *
@@ -27,7 +28,7 @@ public class Vehiculo {
     private String transmision;
     private String traccion; 
     private String tipo;
-    private ArrayList<Oferta> oferta;
+    private ArrayList<Oferta> ofertas;
     private String correo_vendedor;
     private Vendedor vendedor;
     
@@ -86,6 +87,44 @@ public class Vehiculo {
     public double getPrecio() {
         return precio;
     }
+    
+    /* 
+    Metodo para  permitirle al vendedor revisar ofertas del vehiculo;
+    Puede avanzar o retroceder de oferta
+    Puede aceptar la oferta
+    Puede salir 
+    */
+    
+    public void verOfertas(Scanner sc){
+        if(!this.ofertas.isEmpty()){
+            System.out.println("Se han realizado: " + this.ofertas.size());
+            int i = 0;
+            int opcion = 0;
+            while(opcion != 4){
+                System.out.println("Oferta " + (i+1) + "\n" + ofertas.get(i));
+                System.out.println("1 : siguiente oferta\n2: anterior oferta\n3 : aceptar oferta\n4 : salir");
+                opcion = sc.nextInt();
+                if(opcion == 1){
+                    if(i == (this.ofertas.size() - 1))
+                        i = 0;
+                    else
+                        i += 1;       
+                }
+                else if(opcion == 2){
+                    if(i == 0)
+                        i = this.ofertas.size() - 1;
+                    else
+                        i -= 1;
+                }
+                else if(opcion == 3)
+                    this.vendedor.aceptarOferta(i);
+                    this.ofertas.remove(i);
+                    opcion = 4;
+            }
+        }
+        else
+            System.out.println("No hay ofertas para este vehiculo");       
+    }
 
     @Override
     public String toString() {
@@ -105,8 +144,5 @@ public class Vehiculo {
         
         return Objects.equals(this.placa, other.placa);   
     }
-    
-    
-    
-    
+        
 }
