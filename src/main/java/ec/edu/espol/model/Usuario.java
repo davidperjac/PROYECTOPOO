49fb8e4fc1fa4da045aaa367e24e1f80e2f21a10
SDC202 <sebas.decastro@hotmail.com>
2021-07-01 21:40:17 -0500

    Actualicé clases Comprador y Usuario

--- src/main/java/ec/edu/espol/model/Usuario.java
+++ src/main/java/ec/edu/espol/model/Usuario.java
@@ -5,14 +5,116 @@
  */
 package ec.edu.espol.model;
 
+import ec.edu.espol.util.Util;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author davidperez
  */
 public class Usuario {
-    private String correo;
-    private String clave;
-    private String nombres;
-    private String apellidos;
-    private String organizacion;
+    protected int id;
+    protected String correo;
+    protected String clave;
+    protected String nombres;
+    protected String apellidos;
+    protected String organizacion;
+    
+    
+    public Usuario(String correo, String clave, String nombres, String apellidos, String organizacion){
+        this.correo = correo;
+        this.clave = clave;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.organizacion = organizacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(String organizacion) {
+        this.organizacion = organizacion;
+    }
+    
+    // crea lista de correos
+    public static ArrayList<String> recuperarCorreos(String nomfile){
+        ArrayList<String> correos = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(nomfile))){
+            while(sc.hasNextLine()){
+                // linea = correo|...
+                String linea = sc.nextLine();
+                String[] tokens = linea.split("\\|");
+                String correo = tokens[1];
+                correos.add(correo);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return correos;
+    }
+    
+    //valida que el correo sea unico
+    public static boolean validarCorreo(String correo,String nomfile){
+        ArrayList<String> correos = recuperarCorreos(nomfile);
+        return correos.contains(correo);
+    }
+    
+    public static Usuario leerUsuario(Scanner sc, String nomfile){
+        sc.useDelimiter("\n");
+        System.out.println("REGISTRAR UN NUEVO USUARIO");
+        System.out.println( "Introduzca su correo electrónico: " );
+        String correo = sc.next();
+        while(validarCorreo(correo, nomfile)){
+            System.out.println("El correo que ingresó ya posee una cuenta, por favor ingrese otro correo si desea continuar.");
+            correo = sc.next();
+        }
+        System.out.println( "Introduzca una clave: " );
+        String clave = sc.next();
+        System.out.println( "Introduzca sus nombres: " );
+        String nombres = sc.next();
+        System.out.println( "Introduzca sus apellidos: " );
+        String apellidos = sc.next();
+        System.out.println( "Introduzca su organizacion: " );
+        String organizacion = sc.next();
+        Usuario u = new Usuario(correo, clave, nombres, apellidos, organizacion);
+        return u;
+    }
+    
+    
 }
